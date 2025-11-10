0bce8b83d060ccc1e7b0f9e8393dc607b88fdfce
Shivam <satian2990@gmail.com>
2025-11-10 15:30:50 +0530

    Symmetric-ButterflyPattern

--- Symmetric-ButterflyPattern.cpp
+++ Symmetric-ButterflyPattern.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
+
+void symmetricButterflyPattern ()
+{
+	cout << "enter";
+	int count;
+	cin >>  count;
+
+	for (int i = 0; i < count; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			cout << "*";
+		}
+		cout << "\n";
+	}
+}
+
 int main()
 {
-
+	symmetricButterflyPattern();
+	return 0;
 }
---
+++ Symmetric-VoidPatternSource.cpp
@@ -0,0 +1,60 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using  namespace std;
+
+void symmetricVoidPattern ()
+{
+    int n;
+    cout << "enter";
+    cin >> n;
+    char ch = 'A';
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            cout << "*";
+        }
+        for (int j = 0; j < i * 2; j++)
+        {
+            cout << " ";
+        }
+        for (int j = i; j < n; j++)
+        {
+            cout << "*";
+        }
+
+        cout << "\n";
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
+        for (int j = 0; j < n * 2 - i * 2 - 2; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
+
+        cout << "\n";
+    }
+}
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
