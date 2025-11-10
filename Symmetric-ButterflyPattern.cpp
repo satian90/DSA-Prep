f80b9b29a736342c520f7b946b6dc98d1e983108
Shivam <satian2990@gmail.com>
2025-11-10 16:46:28 +0530

    symmetric_ButterFlyPattern(done)

--- Symmetric-ButterflyPattern.cpp
+++ Symmetric-ButterflyPattern.cpp
@@ -4,12 +4,36 @@ using namespace std;
 void symmetricButterflyPattern ()
 {
 	cout << "enter";
-	int count;
-	cin >>  count;
+	int n;
+	cin >>  n;
 
-	for (int i = 0; i < count; i++)
+	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < i; j++)
+		{
+			cout << "*";
+		}
+		for (int j = 0; j < n*2 - i*2 ; j++)
+		{
+			cout << " ";
+		}
+		for (int j = 0; j < i; j++)
+		{
+			cout << "*";
+		}
+		cout << "\n";
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n-i; j++)
+		{
+			cout << "*";
+		}
+		for (int j = 0; j < i * 2 ; j++)
+		{
+			cout << " ";
+		}
+		for (int j = 0; j < n-i; j++)
 		{
 			cout << "*";
 		}
