aba7b8a4c0c98a271531fdf011b27edee6b50c96
Shivam <satian2990@gmail.com>
2025-11-18 17:04:27 +0530

    CountDigitsOptimal

--- CountDigits.cpp
+++ CountDigits.cpp
@@ -3,14 +3,29 @@
 
 using namespace std;
 
-void CountDigits(int n)
+void CountDigitsOptimal(int n)
 {
 	int count = (int)log10(n) + 1;
 
-	cout << "Count:"<< count;
+	cout << "CountOptimal:"<< count<<endl;
+}
+
+void CountDigits(int n)
+{
+	int count = 0;
+	while (n>0)
+	{
+		n = n / 10;
+		count++;
+	}
+	cout << "Count:" << count<<endl;
 }
 
 void main()
 {
-	CountDigits(656654364);
+	cout << "Enter:";
+	int n = 0;
+	cin >> n;
+	CountDigitsOptimal(n);
+	CountDigits(n);
 }
