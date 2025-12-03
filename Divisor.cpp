38a2561bc31ea7400678fdf4efd1b32002904e8c
Shivam <satian2990@gmail.com>
2025-12-03 15:47:33 +0530

    DivisorOptimal

--- Divisor.cpp
+++ Divisor.cpp
@@ -17,15 +17,44 @@ vector<int> divisors(int num)
 	return divisorsList;
 }
 
+vector<int> divisorsOptimal(int num)
+{
+	vector<int> divisorList;
+	for (int i = 1; i * i <= num; i++)
+	{
+		if (num % i == 0)
+		{
+			divisorList.push_back(i);
+
+			if (i != num / i)
+			{
+				divisorList.push_back(num / i);
+			}
+		}
+		
+	}
+
+	return divisorList;
+}
+
+
 void main()
 {
 	int num = 0;
 	cout << "enter:";
 	cin >> num;
 
-	vector<int>divisorsList = divisors(num);
+	vector<int> divisorsList = divisors(num);
+
+	vector<int> divisorListOptimal = divisorsOptimal(num);
+
 	for (int divisor : divisorsList)
 	{
 		cout << divisor << " ";
+	}
+	cout << "Optimal"<< endl;
+	for (int divisorO : divisorListOptimal)
+	{
+		cout << divisorO << " ";
 	}
 }
