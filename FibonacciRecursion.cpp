9a1b98b676f096fe135f829d78b25de651e38a1a
Shivam <satian2990@gmail.com>
2025-12-30 17:33:59 +0530

    Fibonacci Recursion

--- FibonacciRecursion.cpp
+++ FibonacciRecursion.cpp
@@ -1,14 +1,44 @@
 #include<iostream>
 using namespace std;
 
-void fibRecr()
+int fibRecr(int n)
 {
-	int a = 0;
+	/*int last = 1;
+	int secondLast = 0;
+	if (n == 0)
+	{
+		cout << secondLast;
+	}
+	if (n == 1)
+	{
+		cout << secondLast << " " << last;
+	}
+	int c;
+	for (int i = 2; i <= n; i++)
+	{
+		int Num = last + secondLast;
+		last = secondLast;
+		secondLast = Num;
+		cout << Num << " ";
+	}*/
 
-	return 
+	if (n == 0)
+	{
+		return 0;
+	}
+	if (n == 1)
+	{
+		return 1;
+	}
+	return fibRecr(n - 1) + fibRecr(n - 2);
+
 }
 
 void main()
 {
-	fibRecr();
+	int enter=0;
+	cout << "Enter:";
+	cin >> enter;
+	int sumFib = fibRecr(enter);
+	cout << sumFib;
 }
